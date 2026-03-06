36202767a71585f87ccc08fe3b98f7f8e1548452
Riley Paul <rileypaul96@gmail.com>
2026-03-06 15:02:27 -0700

    copy theme generator

---
+++ src/hooks/use-generated-colors.ts
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { generateTheme, generateThemeV3 } from "@/utils/theme-generator";
+import { useColorStore } from "@/store/color-store";
+
+export function useGeneratedColorsV3() {
+  const lightOptions = useColorStore((state) => state.light);
+  const darkOptions = useColorStore((state) => state.dark);
+  const colors = React.useMemo(
+    () => generateThemeV3({ lightOptions, darkOptions }),
+    [lightOptions, darkOptions],
+  );
+  return colors;
+}
+
+export function useGeneratedColorsV4() {
+  const lightOptions = useColorStore((state) => state.light);
+  const darkOptions = useColorStore((state) => state.dark);
+  const colors = React.useMemo(
+    () => generateTheme({ lightOptions, darkOptions }),
+    [lightOptions, darkOptions],
+  );
+  return colors;
+}
